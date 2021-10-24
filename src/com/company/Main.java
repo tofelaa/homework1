ebeca76af8aae1462b639f087631e6fa3c14f547
tofelaa <linnik.alex@gmail.com>
2021-10-24 21:10:11 +0300

    file Manager

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import com.company.Package.AlexCommander;
+
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Sasha");
+    public static void main(String[] args) throws IOException {
+        AlexCommander main = new AlexCommander();
+        main.run();
     }
 }
